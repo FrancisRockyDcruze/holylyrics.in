7f835256d72a5c83790ceedb7f98f5ea5cea273e
FrancisRockyDcruze <87224437+FrancisRockyDcruze@users.noreply.github.com>
2026-03-29 22:43:01 +0530

    Prepare for deployment

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/<repo>/', // <-- replace <repo> with your GitHub repo name
+  base: '/holy_lyrics/',
   plugins: [
     react({
       babel: {
