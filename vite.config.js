7d9c92e006a219a72ccabd130e2df44298779615
FrancisRockyDcruze <87224437+FrancisRockyDcruze@users.noreply.github.com>
2026-03-29 22:23:39 +0530

    package updated

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react({
+//       babel: {
+//         plugins: [['babel-plugin-react-compiler']],
+//       },
+//     }),
+//   ],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/<repo>/', // <-- replace <repo> with your GitHub repo name
   plugins: [
     react({
       babel: {
@@ -10,4 +24,7 @@ export default defineConfig({
       },
     }),
   ],
+  build: {
+    outDir: 'dist', // default, optional
+  },
 })
